bd90bdc49b5aa25a3a9f23ce836d26f401ba0fa1
BhavaniSingam <39860323+BhavaniSingam@users.noreply.github.com>
2021-01-23 15:14:34 +0530

    Update statistics.py

--- statistics.py
+++ statistics.py
@@ -1,4 +1,11 @@
 def calculateStats(numbers):
     
-    avg = numbers/count(numbers)
-    return avg
+if numbers is None:
+return none
+numbers = [1.5,2,3,4]
+avg = sum(numbers) / len(numbers) 
+maximum = max(numbers)
+minimum = min(numbers)
+    
+dict1 = dict({"avg":avg,"max":maximum,"min":minimum})
+    
